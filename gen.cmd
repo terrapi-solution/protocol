b6b85077a86af9fc60d166c68c3b21cb06721e47
Thomas ILLIET <contact@thomas-illiet.fr>
2024-10-13 18:08:28 +0200

    Improve generator script

--- gen.cmd
+++ gen.cmd
@@ -6,7 +6,7 @@ set "PROTO_NAMES=activity deployment"
 
 rem Loop through each element in the array
 for %%i in (%PROTO_NAMES%) do (
-    protoc %%i/*.proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --proto_path=.
+    protoc %%i/%%i.proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --proto_path=.
 
     if ERRORLEVEL 1 (
         echo error processing %%i.proto
