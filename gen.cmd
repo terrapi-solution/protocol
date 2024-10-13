a74e4dff1c9b4f1179b4a71bd8d74d240c727d8b
Thomas ILLIET <contact@thomas-illiet.fr>
2024-10-13 18:06:02 +0200

    Add generator script

---
+++ gen.cmd
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+rem Define array elements
+set "PROTO_NAMES=activity deployment"
+
+rem Loop through each element in the array
+for %%i in (%PROTO_NAMES%) do (
+    protoc %%i/*.proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --proto_path=.
+
+    if ERRORLEVEL 1 (
+        echo error processing %%i.proto
+        exit /b %ERRORLEVEL%
+    )
+)
+
+endlocal
